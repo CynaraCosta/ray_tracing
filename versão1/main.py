4a3081667ed5b53df850d0e6c07a74ca537669d4
CynaraCosta <cvocac@cin.ufpe.br>
2022-10-07 14:59:03 -0300

    added the main and render py file

---
+++ versão1/main.py
@@ -0,0 +1,68 @@
+from distutils.log import info
+from symbol import trailer
+import numpy as np
+from geo_objects import Plane, Sphere, Triangle
+from render import render
+
+def change_type_input(Type):
+    return map(Type, input.split())
+
+def change_type_string(Type, string):
+    return map(Type, string.split())
+
+if __name__ == "__main__":
+
+    # getting the info from txt
+
+    v_res, h_res = change_type_input(int)
+    square_side, dist = change_type_input(float)
+    eye_x, eye_y, eye_z = change_type_input(float)
+    look_at_x, look_at_y, look_at_z = change_type_input(float)
+    up_x, up_y, up_z = change_type_input(float)
+    bg_color_r, bg_color_g, bg_color_b = change_type_input(int)
+
+    how_many_objs = int(input())
+
+    # setting camera
+    eye = (eye_x, eye_y, eye_z)
+    look_at = (look_at_x, look_at_y, look_at_z)
+    up = (up_x, up_y, up_z)
+    bg_color = (bg_color_r, bg_color_g, bg_color_b)
+
+    objs = []
+
+    for obj in range(how_many_objs):
+        which_info = input()
+
+        # checking if is a plane
+        if "/" in info:
+            color, plane = info.split(" / ")
+            color_r, color_g, color_b = change_type_string(int, color)
+            point_origin_x, point_origin_y, point_origin_z, normal_vector_x, normal_vector_y, normal_vector_z = change_type_string(float, plane) 
+
+            new_plane = Plane((point_origin_x, point_origin_y, point_origin_z), (normal_vector_x, normal_vector_y, normal_vector_z))
+            new_plane.set_color((color_r, color_g, color_b))
+            objs.append(new_plane)
+
+        # checking if is a sphere 
+        elif "*" in info:
+            color, sphere = info.split(" * ")
+            color_r, color_g, color_b = change_type_string(int, color)
+            center_x, center_y, center_z, radius = change_type_string(float, sphere)
+
+            new_sphere = Sphere((center_x, center_y, center_z, radius))
+            new_sphere.set_color((color_r, color_g, color_b))
+            objs.append(new_sphere)
+
+        # checking if is a triangle
+        # 63 254 210 > A = (-100.0 100.0 -100.0) B = (100.0 100.0 100.0) C = (100.0 100.0 -100.0)
+        elif ">" in info:
+            color, triangle = info.split(" > ")
+            color_r, color_g, color_b = change_type_string(int, color)
+            point_a_x, point_a_y, point_a_z, point_b_x, point_b_y, point_b_z, point_c_x, point_c_y, point_c_z = change_type_string(float, triangle)
+
+            new_triangle = Triangle((point_a_x, point_a_y, point_a_z), (point_b_x, point_b_y, point_b_z), (point_c_x, point_c_y, point_c_z))
+            new_triangle.set_color = ((color_r, color_g, color_b))
+            objs.append(new_triangle)
+
+    # call the render here
--- versão1/render.py
+++ versão1/render.py
@@ -5,3 +5,40 @@ def normalize(vector):
     return vector / np.linalg.norm(vector)
 
 
+def filter_one(objs, point_O, vector_d):
+    interceptions = []
+
+    for obj in objs:
+        t = obj.intersection(point_O, vector_d)
+
+        if t:
+            # if there is a interception, is going to return the locate = t and which obj it is
+            interceptions.append((t, obj))
+    return interceptions
+
+def filter_two(objs, point_O, vector_d, bg_color):
+    color_to_return = bg_color
+    interceptions = filter_one(objs, point_O, vector_d)
+
+    # sorting to see which is the closest object
+    interceptions.sort()
+
+    if len(interceptions) != 0:
+        # if there is objects at the screen we are going to get this object and see his color
+        closest = interceptions[0][1]
+        color_to_return = closest.color
+
+    # we are going to return this color, because if there is no obj is going to be the bg_color, otherwise the obj_color
+    return color_to_return
+
+def render(v_res, h_res, square_side, dist, eye, look_at, up, bg_color, objs):
+    w = normalize(eye - look_at)
+    u = normalize(np.cross(up, w))
+    v = np.cross(w, u)
+
+    center = eye - (w * dist)
+
+    Qzero = center + (1/2 * square_side * (v_res - 1) * v) - (1/2 * square_side * (h_res - 1) * u)
+
+    j, i = 0, 0
+    Qarb = Qzero + (square_side * (j * u)) - (square_side * (i * v))
