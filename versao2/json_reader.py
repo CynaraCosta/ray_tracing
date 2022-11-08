ad87f949743f3c1a3a00ca601e56a8f9c0e414d3
CynaraCosta <cvocac@cin.ufpe.br>
2022-11-08 09:16:42 -0300

    debugging ray tracer

--- versao2/json_reader.py
+++ versao2/json_reader.py
@@ -37,34 +37,37 @@ def get_path_and_infos(name):
         eye = np.array(data["eye"])
         look_at = np.array(data["look_at"])
         up = np.array(data["up"])
-        background_color = np.array(data["background_color"])
+        background_color = np.array(data["background_color"]) / 255
 
         # getting the objects
         for object in data["objects"]:
             if "plane" in object:
                 plane = Plane(np.array(object["plane"]["sample"]), np.array(object["plane"]["normal"]))
-                plane.set_color(np.array(object["color"]))
+                plane.set_color(np.array(object["color"])/255)
                 plane.set_lights(object['ka'], object['kd'], object['ks'], object['exp'])
-                # get ilumination info
+    
+                objects.append(plane)
 
             if "sphere" in object:
                 sphere = Sphere(np.array(object["sphere"]["center"]), object["sphere"]["radius"])
-                sphere.set_color(np.array(object["color"]))
+                sphere.set_color(np.array(object["color"])/255)
                 sphere.set_lights(object['ka'], object['kd'], object['ks'], object['exp'])
-                # get ilumination info
+                
+                objects.append(sphere)
 
             if "triangle" in object:
                 point_a, point_b, point_c = getting_points(object["triangle"], data)
                 triangle = Triangle(point_a, point_b, point_c)
-                triangle.set_color(np.array(object["color"]))
+                triangle.set_color(np.array(object["color"])/255)
                 triangle.set_lights(object['ka'], object['kd'], object['ks'], object['exp'])
-                # get ilumination info
+                
+                objects.append(triangle)
 
-        ca = np.array(data["ambient_light"])
+        ca = np.array(data["ambient_light"])/255
         lights = []
 
         for light in data["lights"]:
-            lights.append((np.array(light["intensity"]), np.array(light["position"])))
+            lights.append((np.array(light["intensity"])/255, np.array(light["position"])))
         
         image = render.render(v_res, h_res, square_side, dist, eye, look_at, up, background_color, objects, ca, lights)
         plt.imsave(f"images/{name}.png", image)
--- versao2/render.py
+++ versao2/render.py
@@ -54,7 +54,7 @@ def filter_two(objs, point_O, vector_d, bg_color, ca, lights): # cast
         # if there is objects at the screen we are going to get this object and see his color
         t, obj = interceptions[0]
         point = point_O + (t*vector_d)
-        color_to_return = shade(obj, objs, point, -vector_d, obj.normal(point), lights, ca)
+        color_to_return = shade(obj, objs, point, -1 * vector_d, obj.normal(point), lights, ca)
 
     # we are going to return this color, because if there is no obj is going to be the bg_color, otherwise the obj_color
     return color_to_return
@@ -64,19 +64,22 @@ def render(v_res, h_res, square_side, dist, eye, look_at, up, background_color, 
     u = normalize(np.cross(up, w))
     v = np.cross(w, u)
 
-    center = eye - (w * dist)
+    # center = eye - (w * dist)
 
     Q = np.zeros((v_res, h_res, 3))
-    array_pixel = np.zeros((v_res, h_res, 3))
+    # array_pixel = np.zeros((v_res, h_res, 3))
+    img = np.full((v_res, h_res, 3), background_color)
 
-    Q[0,0] = center + (1/2 * square_side * (v_res - 1) * v) - (1/2 * square_side * (h_res - 1) * u)
+    Q[0,0] = eye + (1/2 * square_side * (v_res - 1) * v) - (1/2 * square_side * (h_res - 1) * u)
 
     for i in range(v_res):
         for j in range(h_res):
             Q[i,j] = Q[0,0] + (square_side * (j * u)) - (square_side * (i * v))
             line = normalize(Q[i,j] - eye)
             
-            array_pixel[i, j] = filter_two(objs, eye, line, background_color, ca, lights)
-            
+            placeH = filter_two(objs, eye, line, background_color, ca, lights)
+            placeH = placeH/max(*placeH, 1)
 
-    return array_pixel / 255
+            img[i][j] = placeH            
+
+    return img 
